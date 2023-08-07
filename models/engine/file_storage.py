eec7933e84fecace34dc6626ac8b2bba7e9ffebf
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-08-07 21:38:42 +0000

    added a save function that serializes objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,8 +9,25 @@ class FileStorage:
     __objects = {}
     
     def all(self):
+        """
+        Return all objects
+        Returns: a dictionary of all objects
+        """
         return FileStorage.__objects
     
     def new(self, obj):
+        """
+        Sets a new object
+
+        Args:
+            obj (any): any instance of an object
+        """
         obj_key = type(obj).__name__ + "." + obj.id
         FileStorage.__objects[obj_key] = obj
+
+    def save(self):
+        """ Serializes the objects to a file """
+        obj_to_file = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+        
+        with open(self.__file_path, "w") as file:
+            dump(obj_to_file, file)
