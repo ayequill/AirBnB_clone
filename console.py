0145cebb67ba64bbf67c72800a86222ad31f49e4
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-08-08 00:53:14 +0000

    Added commands
    - Added the quit command
    - Added the EOF marker
    - Added documentation for hep command
    - Ignored all empty commands/lines

--- console.py
+++ console.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 """ This module contains HBNB CLI Interpreter """
-import cmd
+from cmd import Cmd
 
 
-class HBNBCommand(cmd.Cmd):
-    pass
+class HBNBCommand(Cmd):
+    """
+    Class representing the hbnb CLI.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """ Exits the console """
+        return True
+
+    def help_quit(self):
+        """ Quit Command Help """
+        print("Quit command to exit the program\n")
+
+    def do_EOF(self, line):
+        """ End of line marker """
+        print()
+        return True
+
+    def emptyline(self):
+        """ Ignore empty lines """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
