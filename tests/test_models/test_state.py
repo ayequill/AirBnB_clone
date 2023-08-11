7665bb50b3c5e85c2f0d8376b50167850dbcf4c8
Siaw A. Nicholas <54719123+ayequill@users.noreply.github.com>
2023-08-11 21:34:06 +0000

    Added testcases for state class (#26)
    * Added testcases for state class
    
    * Fixed test
    
    * Fixed Pycode

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Test Cases for State """
+from tests.test_models import test_base_model
+from models.state import State
+
+
+class TestState(test_base_model.BaseModelTestCases):
+    """ Test Cases for State """
+
+    def setUp(self):
+        """ Set up """
+        super().setUp()
+        self.state = State()
+
+    def test_state_name(self):
+        """ Test State name """
+        self.state.name = 'Snake land'
+        self.assertIsInstance(self.state.name, str)
+
+    def test_state_to_dict(self):
+        """ Test State to dict """
+        self.state.name = 'Snake land'
+        state_obj = self.state.to_dict()
+        self.assertTrue(self.state.name in state_obj.values())
+
+    # def test_state_name_with_number(self):
+    #     """ Test State name with number """
+    #     with self.assertRaises(ValueError):
+    #         self.state.name = 100
