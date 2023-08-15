cd3c1fa2dffedad881f3dc97408a7a17ec50ece6
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-08-15 00:11:45 +0000

    Modified test cases

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Test Cases for State """
+from unittest import TestCase
 from tests.test_models import test_base_model
 from models.state import State
 
 
-class TestState(test_base_model.BaseModelTestCases):
+class TestState_extend_basemodel(test_base_model.BaseModelTestCases):
     """ Test Cases for State """
 
     def setUp(self):
@@ -12,18 +13,18 @@ class TestState(test_base_model.BaseModelTestCases):
         super().setUp()
         self.state = State()
 
-    def test_state_name(self):
-        """ Test State name """
-        self.state.name = 'Snake land'
-        self.assertIsInstance(self.state.name, str)
-
     def test_state_to_dict(self):
         """ Test State to dict """
         self.state.name = 'Snake land'
         state_obj = self.state.to_dict()
         self.assertTrue(self.state.name in state_obj.values())
 
-    # def test_state_name_with_number(self):
-    #     """ Test State name with number """
-    #     with self.assertRaises(ValueError):
-    #         self.state.name = 100
+
+class TestState(TestCase):
+    """ Test State """
+
+    def test_state_name(self):
+        """ Test State name """
+        new_state = State()
+        new_state.name = 'Snake land'
+        self.assertIsInstance(new_state.name, str)
