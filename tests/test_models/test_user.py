811323b3898a353cdab51539b4969d9ed33111e4
Siaw A. Nicholas <54719123+ayequill@users.noreply.github.com>
2023-08-15 00:31:22 +0000

    Modified tests (#42)

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 """ Test Cases for User class """
+from unittest import TestCase
 from tests.test_models import test_base_model
 from models.user import User
 
 
-class TestUser(test_base_model.BaseModelTestCases):
+class TestUser_with_basemodel(test_base_model.BaseModelTestCases):
     """ Testcases for the user class """
 
     def setUp(self):
         """ Set Up """
         super().setUp()
-        self.user = User()
 
     def tearDown(self):
         """ Cleanup """
         super().tearDown()
 
+
+class TestUser(TestCase):
+    def setUp(self):
+        """ set Up """
+        self.user = User()
+
     def test_user_email(self):
         """ Test user email """
-        self.user.email = 'test@email.com'
+        # self.user.email = 'test@email.com'
         self.assertIsInstance(self.user.email, str)
 
     def test_first_name(self):
         """ Test first name """
-        self.user.first_name = 'Test'
+        # self.user.first_name = 'Test'
         self.assertIsInstance(self.user.first_name, str)
 
     def test_last_name(self):
         """ Test last name """
-        self.user.last_name = 'Case'
+        # self.user.last_name = 'Case'
         self.assertIsInstance(self.user.last_name, str)
 
     def test_password(self):
         """ Test password """
-        self.user.password = 'password'
+        # self.user.password = 'password'
         self.assertIsInstance(self.user.password, str)
