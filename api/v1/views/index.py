965c1411bf15b89b6a53b6e47a14f22e341b9f1f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-27 01:18:48 +0000

    Added a route to return total number of objs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,19 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models.misc import classes
+from models import storage
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    return jsonify({"status": "OK"})
+    """ Returns the status of server """
+    return jsonify({
+        "status": "OK"
+        })
+
+@app_views.route('/stats', methods=['GET'])
+def stat():
+    """ Returns amount of objects in storage """
+    return jsonify({
+        k: storage.count(v) for k, v in classes.items()
+    })
